d319e35e0661e01f6f22a1d277d9c84f91f6f4b2
br-yce <bwimer06@gmail.com>
2024-01-11 08:16:55 -0500

    initializzed

---
+++ src/main/java/me/packwatch/enchantedores/EnchantedOres.java
@@ -0,0 +1,17 @@
+package me.packwatch.enchantedores;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchantedOres extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
