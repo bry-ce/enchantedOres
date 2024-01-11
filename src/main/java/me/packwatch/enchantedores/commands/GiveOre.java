d691d651fbb9cc6c54403fb0a4aa427a08f6cee7
br-yce <bwimer06@gmail.com>
2024-01-11 08:46:53 -0500

    coal implemented, utils etc

---
+++ src/main/java/me/packwatch/enchantedores/commands/GiveOre.java
@@ -0,0 +1,33 @@
+package me.packwatch.enchantedores.commands;
+
+import me.packwatch.enchantedores.items.EnchantedCoal;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+
+
+public class GiveOre implements CommandExecutor {
+    private ItemStack item;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if(!(sender instanceof Player)){return true;}
+        Player p = (Player) sender;
+
+        if(!(p.hasPermission("enchantedores.give.ore"))){ return true;}
+        if(args.length == 1){
+            if (args[0].equalsIgnoreCase("coal")){
+                p.getInventory().addItem(EnchantedCoal.getEnchCoal());
+            }
+
+        }
+
+
+        return true;
+    }
+}
