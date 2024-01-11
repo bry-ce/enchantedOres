00480d58fd0664c71ffa9b49704acf5c2471b0b2
br-yce <bwimer06@gmail.com>
2024-01-11 08:52:37 -0500

    registered cmd

--- src/main/java/me/packwatch/enchantedores/EnchantedOres.java
+++ src/main/java/me/packwatch/enchantedores/EnchantedOres.java
@@ -1,12 +1,13 @@
 package me.packwatch.enchantedores;
 
+import me.packwatch.enchantedores.commands.GiveOre;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnchantedOres extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getCommand("giveore").setExecutor(new GiveOre());
     }
 
     @Override
