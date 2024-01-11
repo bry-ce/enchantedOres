e6c9b63b1df28e684031e5439c342aa7438ec6fe
br-yce <bwimer06@gmail.com>
2024-01-11 09:01:04 -0500

    added all ores

--- src/main/java/me/packwatch/enchantedores/commands/GiveOre.java
+++ src/main/java/me/packwatch/enchantedores/commands/GiveOre.java
@@ -1,14 +1,11 @@
 package me.packwatch.enchantedores.commands;
 
-import me.packwatch.enchantedores.items.EnchantedCoal;
-import me.packwatch.enchantedores.items.EnchantedIron;
+import me.packwatch.enchantedores.items.EnchantedItems;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Arrays;
 
 
 public class GiveOre implements CommandExecutor {
@@ -23,9 +20,17 @@ public class GiveOre implements CommandExecutor {
         if(!(p.hasPermission("enchantedores.give.ore"))){ return true;}
         if(args.length == 1){
             if (args[0].equalsIgnoreCase("coal")){
-                p.getInventory().addItem(EnchantedCoal.getEnchCoal());
+                p.getInventory().addItem(EnchantedItems.getEnchCoal());
             } else if (args[0].equalsIgnoreCase("iron")){
-                p.getInventory().addItem(EnchantedIron.getEnchIron());
+                p.getInventory().addItem(EnchantedItems.getEnchIron());
+            } else if (args[0].equalsIgnoreCase("gold")){
+                p.getInventory().addItem(EnchantedItems.getEnchGold());
+            } else if (args[0].equalsIgnoreCase("redstone")){
+                p.getInventory().addItem(EnchantedItems.getEnchRedstone());
+            } else if (args[0].equalsIgnoreCase("diamond")){
+                p.getInventory().addItem(EnchantedItems.getEnchDiamond());
+            } else if (args[0].equalsIgnoreCase("amethyst")){
+                p.getInventory().addItem(EnchantedItems.getEnchAmethyst());
             }
 
         }
---
+++ src/main/java/me/packwatch/enchantedores/items/EnchantedItems.java
@@ -0,0 +1,101 @@
+package me.packwatch.enchantedores.items;
+
+import me.packwatch.enchantedores.utils.Utils;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class EnchantedItems {
+
+    public static ItemStack getEnchCoal(){
+        ItemStack i = new ItemStack(Material.COAL);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Coal"));
+
+        return i;
+    }
+
+    public static ItemStack getEnchIron(){
+        ItemStack i = new ItemStack(Material.IRON_INGOT);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Iron Ingot"));
+
+        return i;
+    }
+
+    public static ItemStack getEnchGold(){
+        ItemStack i = new ItemStack(Material.GOLD_INGOT);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Gold Ingot"));
+
+        return i;
+    }
+
+    public static ItemStack getEnchRedstone(){
+        ItemStack i = new ItemStack(Material.REDSTONE);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Redstone"));
+
+        return i;
+    }
+
+    public static ItemStack getEnchDiamond(){
+        ItemStack i = new ItemStack(Material.DIAMOND);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Diamond"));
+
+        return i;
+    }
+
+    public static ItemStack getEnchAmethyst(){
+        ItemStack i = new ItemStack(Material.AMETHYST_SHARD);
+
+        ItemMeta m = i.getItemMeta();
+
+        m.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        i.setItemMeta(m);
+        m.setDisplayName(Utils.chat("&aEnchanted Amethyst"));
+
+        return i;
+    }
+}
